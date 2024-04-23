e6b7f4b6f2db9160d925bece23731f10d4b2b0bc
lucartc <lucartc@protonmail.com>
2024-04-23 12:39:36 -0300

    Adds test support

---
+++ test.js
@@ -0,0 +1,11 @@
+let fs = require('fs/promises')
+let iso = require('./index.js')
+let file = fs.readFile('./waves.mp4')
+let boxes = []
+
+// Basic test case, where the api methods are tested against a file
+file
+.then(data => {
+    boxes = iso.tree(data)
+    iso.fix(data,'./out.mp4')
+})
